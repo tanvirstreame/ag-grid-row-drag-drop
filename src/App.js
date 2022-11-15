9aff4e8bd2ea6763388fcc3a9b51f4c447150cae
Tanvir Islam Streame <engr.tanvir.islam.1@gmail.com>
2022-11-16 00:03:10 +0600

    Row dragging managed way

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { render } from 'react-dom';
+import { AgGridReact } from 'ag-grid-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
+
+const App = () => {
+   const [rowData] = useState([
+       {make: "Toyota", model: "Celica", price: 35000},
+       {make: "Ford", model: "Mondeo", price: 32000},
+       {make: "Porsche", model: "Boxster", price: 72000}
+   ]);
+   
+   const [columnDefs] = useState([
+       { field: 'make', rowDrag: true },
+       { field: 'model' },
+       { field: 'price' }
+   ])
+
+   return (
+       <div className="ag-theme-alpine" style={{height: 400, width: 600}}>
+           <AgGridReact
+               rowData={rowData}
+               rowDragManaged={true}
+               columnDefs={columnDefs}>
+               
+           </AgGridReact>
+       </div>
+   );
+};
 
 export default App;
